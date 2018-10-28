f04a6c26763bf57c85c775af49c652228dade7e8
tjdombrowski <tjdombrowski@madisoncollege.edu>
2018-10-27 21:17:04 -0500

    Fixing the test to unmarshall the results.

--- src/main/java/gov/noaa/DwmlType.java
+++ src/main/java/gov/noaa/DwmlType.java
@@ -1,11 +1,7 @@
 
 package gov.noaa;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 
 
 /**
@@ -28,6 +24,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  * 
  */
+@XmlRootElement(name="dwml")
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "dwmlType", propOrder = {
     "latLonList"
--- src/test/java/gov/noaa/NdfdXMLBindingStubTest.java
+++ src/test/java/gov/noaa/NdfdXMLBindingStubTest.java
@@ -2,6 +2,10 @@ package gov.noaa;
 
 import org.junit.jupiter.api.Test;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NdfdXMLBindingStubTest {
@@ -10,9 +14,15 @@ class NdfdXMLBindingStubTest {
     void latLonListZipCode() throws Exception {
         NdfdXMLBindingStub binding = (NdfdXMLBindingStub)new NdfdXMLLocator().getndfdXMLPort();
 
-        String result = binding.latLonListZipCode("53714");
+        JAXBContext jaxbContext = JAXBContext.newInstance(DwmlType.class);
 
-        assertEquals("", result);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+        String response = binding.latLonListZipCode("53714");
+
+        DwmlType dwml = (DwmlType) jaxbUnmarshaller.unmarshal(new StringReader(response)); //
+
+        assertEquals("43.0798,-89.3875", dwml.getLatLonList());
     }
 
 }
